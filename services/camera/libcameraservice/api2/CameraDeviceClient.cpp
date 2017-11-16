halogenOS/android_frameworks_av

libcameraservice: add a flag for preventing stream rotation
Change-Id: I572f55308de7bde021372941ac44d1e4031e1fab

services/camera/libcameraservice/api2/CameraDeviceClient.cpp
@@ -554,8 +554,10 @@ binder::Status CameraDeviceClient::createStream(
                 " (%d x %d) with format 0x%x.",
               __FUNCTION__, mCameraId, streamId, width, height, format);
 
+#ifdef CAMERA_STREAM_NO_ROTATION
         // Set transform flags to ensure preview to be rotated correctly.
         res = setStreamTransformLocked(streamId);
+#endif
 
         *newStreamId = streamId;
     }

HexagonRom/frameworks_av

services/camera/libcameraservice/api2/CameraDeviceClient.cpp
@@ -554,8 +554,10 @@ binder::Status CameraDeviceClient::createStream(
                 " (%d x %d) with format 0x%x.",
               __FUNCTION__, mCameraId, streamId, width, height, format);
 
+#ifndef CAMERA_STREAM_NO_ROTATION
         // Set transform flags to ensure preview to be rotated correctly.
         res = setStreamTransformLocked(streamId);
+#endif
 
         *newStreamId = streamId;
     }